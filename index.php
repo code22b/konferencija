42333072bf7dc56a19cda4cdc151040a38f0e06f
code22b <pasalicbarisa@gmail.com>
2024-11-06 23:02:52 +0100

    Struktura Git projekta

--- index.php
+++ index.php
@@ -1,1 +1,8 @@
-<h1>Hello</h1>
+<?php 
+
+include 'konekcija.php';
+$baza = "SELECT * FROM predavaci";
+$upit = mysqli_query($konekcija, $baza);
+$rezultat = mysqli_fetch_all($upit, MYSQLI_ASSOC);
+
+include 'index.prikaz.php';
